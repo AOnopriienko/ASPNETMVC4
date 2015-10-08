045b0dd042b5030ba3fe97c2b028c75ff73137a4
A.Onopriienko <a.onopriienko@gmail.com>
2015-10-08 18:37:56 +0300

    Start New project ASP.NET MVC 4

---
+++ Lesson1/App_Start/PreStartApp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+[assembly: WebActivatorEx.PreApplicationStartMethod(typeof(Lesson1.App_Start.PreStartApp), "Start")]
+namespace Lesson1.App_Start
+{
+    public static class PreStartApp
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        /// Метод запускается один раз перед стартом приложения        
+        /// </summary>
+        public static void Start()
+        {
+            logger.Info("Application PreStart");
+        }
+    }
+}
